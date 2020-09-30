cc3e1c97592cb7b1eac41431e9b78c8ca004d319
AaravSrivastav <22asrivastav@athenian.org>
2020-09-30 00:35:37 +0000

    added array methods to myMain

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,24 +2,62 @@ public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] arr1 = new int[arr.length];
+        for(int i = 0; i<arr.length; i++)
+        {
+            arr1[i]=arr[(arr.length - i-1)];
+        }
+        return arr1;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++)
+        {
+          if(arr[i] > max)
+           {
+             max = arr[i];
+           }
+        }
+        int newMax = max;
+        max = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++)
+        {
+          if(arr[i] > max && arr[i] < newMax)
+           {
+             max = arr[i];
+           }
+        }
+        return max;
     }
 
     // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        boolean var = true;
+        for(int i = 1; i<arr.length-1; i++)
+        {
+            double doub1 = (double)(arr[i-1]);
+            double doub2 = (double)(arr[i]);
+            double doub3 = (double)(arr[i+1]);
+            if((doub1/doub2) == (doub2/doub3))
+            {
+                var = true;
+            }
+            else
+            {
+                var = false;
+                break;
+            }
+        }
+        return var;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println(reverse(arr));
+        System.out.println(secondLargest(arr));
+        System.out.println(isGeometric(arr));
     }
 }
